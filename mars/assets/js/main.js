08c4265f4dbfee9f1396c104b2596e74479e752c
ShaniaRoseline <ShaniaRoseline@users.noreply.github.com>
2020-06-19 01:24:04 +1000

    update temporal loop

---
+++ mars/assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	console.log("I'm Ready")
+
+	$('.h-1').click(function(){
+		$('.t-1').slideToggle("fast");
+	});
+
+	$('.h-2').click(function(){
+		$('.t-2').slideToggle("fast");
+	});
+ 	
+ 	$('.h-3').click(function(){
+		$('.t-3').slideToggle("fast");
+	});
+
+	$('.h-4').click(function(){
+		$('.t-4').slideToggle("fast");
+	});
+
+	$('.h-5').click(function(){
+		$('.t-5').slideToggle("fast");
+	});
+
+	$('.h-6').click(function(){
+		$('.t-6').slideToggle("fast");
+	});
+});
+
