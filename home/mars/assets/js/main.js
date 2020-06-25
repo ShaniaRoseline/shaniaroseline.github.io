f3685c7300d7b69153f3e39e5acdab7d3fcac30c
ShaniaRoseline <ShaniaRoseline@users.noreply.github.com>
2020-06-26 01:58:22 +1000

    update

--- home/mars/assets/js/main.js
+++ home/mars/assets/js/main.js
@@ -2,27 +2,31 @@ $(document).ready(function(){
 	console.log("I'm Ready")
 
 	$('.h-1').click(function(){
-		$('.t-1').slideToggle("fast");
+		$('.t-1').slideToggle("slow");
 	});
 
 	$('.h-2').click(function(){
-		$('.t-2').slideToggle("fast");
+		$('.t-2').slideToggle("slow");
 	});
  	
  	$('.h-3').click(function(){
-		$('.t-3').slideToggle("fast");
+		$('.t-3').slideToggle("slow");
 	});
 
 	$('.h-4').click(function(){
-		$('.t-4').slideToggle("fast");
+		$('.t-4').slideToggle("slow");
 	});
 
 	$('.h-5').click(function(){
-		$('.t-5').slideToggle("fast");
+		$('.t-5').slideToggle("slow");
 	});
 
 	$('.h-6').click(function(){
-		$('.t-6').slideToggle("fast");
+		$('.t-6').slideToggle("slow");
+	});
+
+	$('.h-7').click(function(){
+		$('.t-7').slideToggle("slow");
 	});
 });
 
